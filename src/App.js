1888793083a7f3ab0aa28f7ffa72bbd70af3299f
Narasimha <narasimhav@virtuositycg.com>
2020-12-12 22:24:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
-				<p>Rendering react start page.</p>
+				<p>CI test using travis CI</p>
 				<a
 					className='App-link'
 					href='https://reactjs.org'
